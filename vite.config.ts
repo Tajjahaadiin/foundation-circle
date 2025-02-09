ee4b06a5e9550635c67e5d0d76463e1282f2ff37
Tajjuddiin <tajjuddiinauliya@gmail.com>
2025-02-09 21:38:19 +0700

    upgrade to use react router-dom features

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 /// <reference types="vitest" />
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import react from '@vitejs/plugin-react';
+import { reactRouter } from '@react-router/dev/vite';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    // react()
+    reactRouter(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
